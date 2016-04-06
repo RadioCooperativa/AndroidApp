eba552ad44299611795bf71280140dbf3990a9f1
Rodrigo Vargas <rvargas@cooperativa.cl>
2016-04-06 18:18:18 -0300

    Agrego sección de página al 85% del tamaño y botón al 15% restante

--- app/src/main/java/com/example/innova6/cooperativa/MainActivity.java
+++ app/src/main/java/com/example/innova6/cooperativa/MainActivity.java
@@ -2,14 +2,41 @@ package com.example.innova6.cooperativa;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
+
+    public String lk;
+    public static boolean flag = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // recogerparametro();
+       populateWebView();
+    }
+    private void populateWebView() {
+        lk= "http://m.cooperativa.cl";
+        Log.i("La url ", "es: " + lk);
+        final WebView myWebView;
+        myWebView = new WebView(this);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.loadUrl(lk);
+        setContentView(myWebView);
+        myWebView.setWebViewClient(new WebViewClient() {
+            public void onPageFinished(WebView view, String url) {
+                if (url.contains("#") && flag == false) {
+                    myWebView.loadUrl(url);
+                    flag = true;
+                } else {
+                    flag = false;
+                }
+            }
+        });
     }
 
 }
